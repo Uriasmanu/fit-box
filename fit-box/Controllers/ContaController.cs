031ff0cf912a0e5e20ca4ce0f70ef4bf049b0dc7
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-08-20 19:45:02 -0300

    Adição do metodo de geração de token JWT

--- fit-box/Controllers/ContaController.cs
+++ fit-box/Controllers/ContaController.cs
@@ -1,9 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using fit_box.Models;
+using fit_box.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace fit_box.Controllers
 {
-    public class ContaController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContaController : ControllerBase
     {
-       
+        private readonly LoginService _loginServices;
+        private readonly string _chaveSecreta = "e3c46810-b96e-40d9-a9eb-9ffa7b373e5b"; 
+
+        public ContaController(LoginService loginServices)
+        {
+            _loginServices = loginServices;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login([FromBody] Login login)
+        {
+            var user = await _loginServices.AuthenticateUserAsync(login.Username, login.Password);
+
+            if (user == null)
+            {
+                return Unauthorized(new { message = "Credenciais inválidas." });
+            }
+
+            var token = GerarTokenJWT(user);
+            return Ok(new { token });
+        }
+
+        private string GerarTokenJWT(Login login)
+        {
+            var chave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_chaveSecreta));
+            var credencial = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+        new Claim(ClaimTypes.NameIdentifier, login.Id.ToString()), 
+        new Claim(ClaimTypes.Name, login.Username) 
+    };
+
+            var token = new JwtSecurityToken(
+                issuer: "your_issuer",
+                audience: "your_audience",
+                claims: claims,
+                expires: DateTime.Now.AddHours(8),
+                signingCredentials: credencial
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
     }
 }
--- fit-box/Program.cs
+++ fit-box/Program.cs
@@ -1,12 +1,47 @@
 using fit_box.Data;
 using fit_box.Service;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+string chaveSecreta = "e3c46810-b96e-40d9-a9eb-9ffa7b373e5b";
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure the database connection
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer("Server=tcp:fit-box-server.database.windows.net,1433;Initial Catalog=sql-fit-box;Persist Security Info=False;User ID=manoela;Password=senha123#;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
+    options.UseSqlServer("Server=tcp:fit-box-server.database.windows.net,1433;Initial Catalog=sql-fit-box;Persist Security Info=False;User ID=manoela;Password=senha123#;Encrypt=True;TrustServerCertificate=False;Connection Timeout=60;"));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = "system_tasks",
+        ValidAudience = "seus_usuarios",
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(chaveSecreta))
+    };
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        policy =>
+        {
+            policy.WithOrigins()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+
+        });
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -29,6 +64,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseCors();
 
 app.UseAuthorization();
 
